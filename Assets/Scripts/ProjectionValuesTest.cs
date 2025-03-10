1ed3850a5eaa9c851fc663880b8a887dd877879d
Fae <fae.burns0@gmail.com>
2025-03-10 18:23:10 +0000

    projection values testing and extraction

---
+++ Assets/Scripts/ProjectionValuesTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class ProjectionValuesTest : MonoBehaviour
+{
+    private void Update()
+    {
+        Camera mainCamera = Camera.main!;
+
+        Matrix4x4 projectionLeft = mainCamera.GetStereoProjectionMatrix(Camera.StereoscopicEye.Left);
+        Matrix4x4 projectionRight = mainCamera.GetStereoProjectionMatrix(Camera.StereoscopicEye.Right);
+
+        Matrix4x4 viewLeft = mainCamera.GetStereoViewMatrix(Camera.StereoscopicEye.Left);
+        Matrix4x4 viewRight = mainCamera.GetStereoViewMatrix(Camera.StereoscopicEye.Right);
+
+        float ipd = mainCamera.stereoSeparation;
+
+        Debug.LogWarning("New Frame");
+
+        Debug.Log(projectionLeft.ToString());
+        Debug.Log(projectionRight.ToString());
+
+        Debug.Log(ipd);
+
+        Debug.Log(viewLeft.ToString());
+        Debug.Log(viewRight.ToString());
+    }
+}
